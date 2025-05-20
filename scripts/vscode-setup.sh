78963b9d0480d3a7290ea6eb8141dde27ac7e031
medwf <med.wf95@gmail.com>
2025-05-20 15:34:43 +0100

    add config kitty and waybar

--- scripts/vscode-setup.sh
+++ scripts/vscode-setup.sh
@@ -4,11 +4,11 @@
 mkdir -p ~/.config/Code/User/
 
 # Symlink or copy config files
-ln -sf ~/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
-ln -sf ~/dotfiles/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
-cp -r ~/dotfiles/.config/Code/User/snippets ~/.config/Code/User/
+ln -sf ~/Repository/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
+ln -sf ~/Repository/dotfiles/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
+cp -r ~/Repository/dotfiles/.config/Code/User/snippets ~/.config/Code/User/
 
 # Install extensions
 while read -r extension; do
     code --install-extension "$extension"
-done < ~/dotfiles/scripts/vscode-extensions.txt
+done < ~/Repository/dotfiles/scripts/vscode-extensions.txt
