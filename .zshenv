d04d1ead412a993010e8e311ea1bb1e6a9870e93
medwf <med.wf95@gmail.com>
2026-07-07 00:07:25 +0100

    add tmux

--- .zshenv
+++
@@ -1,6 +0,0 @@
-export PATH="/opt/flutter/bin:$PATH"
-export CHROME_EXECUTABLE="/usr/bin/brave"
-
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
--- backup.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-cp -rf ~/.config/hypr config/
-cp -rf ~/.config/kitty config/
-cp -rf ~/.config/noctalia config/
-cp -rf ~/.config/btop config/
-cp -rf ~/.config/nvim config/
-cp -rf ~/.config/Thunar config/
-cp -rf ~/.config/yazi config/
-
